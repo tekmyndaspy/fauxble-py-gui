35bd4cc9c3a6631b166f644e11938b9598fe3c9e
Sadie Kauffman <tekmyndaspy@gmail.com>
2025-02-17 23:43:40 -0500

    fix update.bat

--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
 :: running this file will update fauxble, provided that it is already initialized as a git repo
-git reset --hard &:: this will reset any changes made between now and the last pull
-git pull &:: this will make it such that the local repo matches the remote repo
+git reset --hard
+git pull
